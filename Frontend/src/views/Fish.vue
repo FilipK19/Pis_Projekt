13221d369d3f9182ba9ee7163c032a4be64d7edd
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-06-24 21:55:02 +0200

    Dugme za random ribu

--- Frontend/src/views/Fish.vue
+++ Frontend/src/views/Fish.vue
@@ -30,7 +30,7 @@
             >
           </v-btn>
           <v-col> </v-col>
-          <p>Random Number = {{ ranFish }}</p>
+          <p>Random Number = {{ randomNumber }}</p>
           <v-btn flat color="#98FB98" @click="randomNum()">Randomize</v-btn>
         </v-col>
         <v-col>
@@ -76,20 +76,54 @@ export default {
     submit() {
       console.log(this.input);
     },
-    myFunction: function () {
-      this.randomNumber = Math.random() * 10;
-    },
+
     randomNum: function () {
-      this.ranFish = Math.floor(Math.random() * (21 - 1 + 1) + 1);
+      this.randomNumber = Math.floor(Math.random() * (21 - 1 + 1) + 1);
     },
 
     randomFish() {
-      if (this.randomNumber < 1) {
+      if (this.randomNumber == 1) {
         this.rand = "Pastrva";
-      } else if (this.randomNumber > 1 && this.randomNumber < 2) {
-        this.rand = "Srdela";
-      } else if (this.randomNumber > 2 && this.randomNumber < 3) {
-        this.rand = "Oslić";
+      } else if (this.randomNumber == 2) {
+        this.rand = "Brancin";
+      } else if (this.randomNumber == 3) {
+        this.rand = "Orada";
+      } else if (this.randomNumber == 4) {
+        this.rand = "Zubatac";
+      } else if (this.randomNumber == 5) {
+        this.rand = "Cipal";
+      } else if (this.randomNumber == 6) {
+        this.rand = "Škrapina";
+      } else if (this.randomNumber == 7) {
+        this.rand = "Arbun";
+      } else if (this.randomNumber == 8) {
+        this.rand = "Trilja";
+      } else if (this.randomNumber == 9) {
+        this.rand = "Ugor";
+      } else if (this.randomNumber == 10) {
+        this.rand = "Morska Mačka";
+      } else if (this.randomNumber == 11) {
+        this.rand = "Skuša";
+      } else if (this.randomNumber == 12) {
+        this.rand = "Šaran";
+      } else if (this.randomNumber == 13) {
+        this.rand = "Tuna";
+      } else if (this.randomNumber == 14) {
+        this.rand = "Špar";
+      } else if (this.randomNumber == 15) {
+        this.rand = "Šaran";
+      } else if (this.randomNumber == 16) {
+        this.rand = "Som";
+      } else if (this.randomNumber == 17) {
+        this.rand = "Smuđ";
+      } else if (this.randomNumber == 18) {
+        this.rand = "Štuka";
+      } else if (this.randomNumber == 19) {
+        this.rand = "Kečiga";
+      } else if (this.randomNumber == 20) {
+        this.rand = "Lipljen";
+      } else if (this.randomNumber == 21) {
+        this.rand = "Mladica";
       }
     },
   },
